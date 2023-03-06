a2ff6135258ff235fb3d86d140cbab92b31dbbfa
Santiago Pujol <santiagopujol7@gmail.com>
2023-03-06 17:57:02 +0000

    Added register request

--- internal/users/api.go
+++ internal/users/api.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -18,8 +19,19 @@ func (h *usersHandler) RegisterRoutes(r *chi.Mux) {
 	r.Post("/api/users/login", h.handleLogin)
 }
 
-func (h *usersHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
+type registerRequest struct {
+	User struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	} `json:"user"`
+}
 
+func (h *usersHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	request := registerRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+	}
 }
 
 func (h *usersHandler) handleLogin(w http.ResponseWriter, r *http.Request) {
